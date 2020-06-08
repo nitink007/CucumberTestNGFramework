39b68fa3af65ecea3b62466910b1320273cf73d6
nitink007 <nitinkumar1992.delhi@gmail.com>
2020-06-09 01:06:56 +0530

    Added @CucumerOptions plugin to add reporting

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.*;
 
-@CucumberOptions(features = "src/test/resources/features", glue = "steps", strict = true)
+@CucumberOptions(features = "src/test/resources/features", glue = "steps", strict = true,
+        plugin = {"pretty","html:target/cucumber-reports/html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
