5ee00cca0c67acd9aa3aeb990a8239743d938ca0
nitink007 <nitinkumar1992.delhi@gmail.com>
2020-06-13 23:28:28 +0530

    WIP: Devtools Support

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,12 +1,18 @@
 package runner;
 
 
+import com.google.common.collect.ImmutableList;
 import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.network.Network;
+import org.openqa.selenium.devtools.network.model.BlockedReason;
+import org.openqa.selenium.devtools.network.model.ResourceType;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 @CucumberOptions(features = "src/test/resources",
@@ -14,17 +20,35 @@ import org.testng.annotations.*;
                 plugin = {"pretty","json:target/cucumber-reports/cucumber.json","html:target/cucumber-html-reports"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    WebDriver driver;
+    ChromeDriver driver;
 
     @BeforeSuite
     public void beforeSuit() {
         System.out.println("From testNG before suit");
         WebDriverManager.chromedriver().setup();
-
         driver = new ChromeDriver();
-        driver.get("https://google.com");
         driver.manage().window().maximize();
 
+        DevTools devTools = driver.getDevTools();
+
+//        //enable Network
+//        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+//
+//        //set blocked URL patterns
+//        devTools.send(Network.setBlockedURLs(ImmutableList.of("*.css", "*.png")));
+//
+//        //add event listener to verify that css and png are blocked
+//        devTools.addListener(Network.loadingFailed(), loadingFailed -> {
+//
+//            if (loadingFailed.getResourceType().equals(ResourceType.Stylesheet)) {
+//                Assert.assertEquals(loadingFailed.getBlockedReason(), BlockedReason.inspector);
+//            }
+//
+//            else if (loadingFailed.getResourceType().equals(ResourceType.Image)) {
+//                Assert.assertEquals(loadingFailed.getBlockedReason(), BlockedReason.inspector);
+//            }
+//
+//        });
 
     }
 
--- src/test/resources/features/Sprinklr/AdsComposer/TestFeature1.feature
+++ src/test/resources/features/Sprinklr/AdsComposer/TestFeature1.feature
@@ -1,13 +1,13 @@
-Feature: This is sample feature from inner folder
-
-  Scenario: This sample scenario1
-    Given User open "nitin" application
-    And user clicks on "sample1" button
-
-  Scenario: This sample scenario2
-    Given User open "nitin2" application
-    And user clicks on "sample2" button
-
-  Scenario: This sample scenario3
-    Given User open "nitin3" application
-    And user clicks on "sample3" button
+#Feature: This is sample feature from inner folder
+#
+#  Scenario: This sample scenario1
+#    Given User open "nitin" application
+#    And user clicks on "sample1" button
+#
+#  Scenario: This sample scenario2
+#    Given User open "nitin2" application
+#    And user clicks on "sample2" button
+#
+#  Scenario: This sample scenario3
+#    Given User open "nitin3" application
+#    And user clicks on "sample3" button
