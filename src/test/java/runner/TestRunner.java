046eac56e62420bb631624e0eeeb81c5a6d9195e
nitink007 <nitinkumar1992.delhi@gmail.com>
2020-07-04 19:05:47 +0530

    WIP: Devtools Support

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,33 +6,60 @@ import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.network.Network;
 import org.openqa.selenium.devtools.network.model.BlockedReason;
 import org.openqa.selenium.devtools.network.model.ResourceType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
 
 @CucumberOptions(features = "src/test/resources",
                 glue = "steps", strict = true,
                 plugin = {"pretty","json:target/cucumber-reports/cucumber.json","html:target/cucumber-html-reports"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    ChromeDriver driver;
+    RemoteWebDriver driver;
 
     @BeforeSuite
-    public void beforeSuit() {
+    public void beforeSuit() throws MalformedURLException {
         System.out.println("From testNG before suit");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//
+//        DevTools devTools = driver.getDevTools();
 
-        DevTools devTools = driver.getDevTools();
+
+        // ----------------------- Experiment -----------------------
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        dc.setBrowserName("chrome");
+        dc.setPlatform(Platform.LINUX);
+
+        URL url = new URL("http://localhost:4444/wd/hub");
+
+        driver = new RemoteWebDriver(url, dc);
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+
+
 
 //        //enable Network
-//        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+//        devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+//
+//        devTools.addListener(responseRecieved(), responseRecieved -> {
+//            Assert.assertNotNull(responseRecieved.ge);
+//        });
 //
 //        //set blocked URL patterns
 //        devTools.send(Network.setBlockedURLs(ImmutableList.of("*.css", "*.png")));
