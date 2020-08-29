a5fce29b52b19d9678e304f4e31f2530911a373c
nitink007 <nitinkumar1992.delhi@gmail.com>
2020-08-29 18:28:42 +0530

    Updated Maven-Cucumber-Reporting plugin

--- src/test/java/steps/Feature1StepDef.java
+++ src/test/java/steps/Feature1StepDef.java
@@ -10,7 +10,7 @@ public class Feature1StepDef {
     @Given("User open {string} application")
     public void userOpenApplication(String arg0) {
         if (arg0.equals("nitin2"))
-            Assert.fail("known failure - Fail build");
+            Assert.fail("known failure");
     }
 
     @And("user clicks on {string} button")
